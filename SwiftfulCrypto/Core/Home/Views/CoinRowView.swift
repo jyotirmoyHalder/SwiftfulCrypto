94aec1755a030414b9db0091778b7c72150b89fb
Jyotirmoy Halder <jyotirmoy.cse5.bu@gmail.com>
2025-08-26 10:04:50 +0600

    [Feature] CoinRowView, DeveloperPreview, Double added

---
+++ SwiftfulCrypto/Core/Home/Views/CoinRowView.swift
@@ -0,0 +1,77 @@
+//
+//  CoinRowView.swift
+//  SwiftfulCrypto
+//
+//  Created by jyotirmoy_halder on 25/8/25.
+//
+
+import SwiftUI
+
+struct CoinRowView: View {
+    
+    let coin: CoinModel
+    let showHoldingsColumn: Bool
+    
+    var body: some View {
+        HStack(spacing: 0) {
+            leftColumn
+            Spacer()
+            if showHoldingsColumn {
+                centerColumn
+            }
+            rightColumn
+        }
+        .font(.subheadline)
+    }
+}
+
+#Preview(traits: .sizeThatFitsLayout) {
+    CoinRowView(coin: DeveloperPreview.dev.coin, showHoldingsColumn: true)
+        .preferredColorScheme(.light)
+}
+
+#Preview(traits: .sizeThatFitsLayout) {
+    CoinRowView(coin: DeveloperPreview.dev.coin, showHoldingsColumn: true)
+            .preferredColorScheme(.dark)
+}
+
+
+
+extension CoinRowView {
+    
+    private var leftColumn: some View {
+        HStack(spacing: 0) {
+            Text("\(coin.rank)")
+                .font(.caption)
+                .foregroundStyle(Color.theme.secondaryText)
+                .frame(minWidth: 30)
+            Circle()
+                .frame(width: 30, height: 30)
+            Text(coin.symbol.uppercased())
+                .font(.headline)
+                .padding(.leading, 6)
+                .foregroundStyle(Color.theme.accent)
+        }
+    }
+    
+    private var centerColumn: some View {
+        VStack(alignment: .trailing) {
+            Text(coin.currentHoldingsValue.asCurrencyWith2Decimals())
+                .bold()
+            Text((coin.currentHoldings ?? 0).asNumberString())
+        }
+        .foregroundStyle(Color.theme.accent)
+    }
+    
+    private var rightColumn: some View {
+        VStack(alignment: .trailing) {
+            Text(coin.currentPrice.asCurrencyWith2Decimals())
+                .bold()
+                .foregroundStyle(Color.theme.accent)
+            Text(coin.priceChangePercentage24H?.asPercentString() ?? "")
+                .foregroundStyle((coin.priceChangePercentage24H ?? 0) >= 0 ?
+                                 Color.theme.green : Color.theme.red)
+        }
+        .frame(width: UIScreen.main.bounds.width / 3.5, alignment: .trailing)
+    }
+}
