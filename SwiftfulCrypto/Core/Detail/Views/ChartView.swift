46265e2338d174a376ef02943fd4de384ede217f
Jyotirmoy Halder <jyotirmoy.cse5.bu@gmail.com>
2025-08-31 16:49:13 +0600

    [Feature] Animated Chart View Added

---
+++ SwiftfulCrypto/Core/Detail/Views/ChartView.swift
@@ -0,0 +1,18 @@
+//
+//  ChartView.swift
+//  SwiftfulCrypto
+//
+//  Created by jyotirmoy_halder on 31/8/25.
+//
+
+import SwiftUI
+
+struct ChartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChartView()
+}
---
+++ SwiftfulCrypto/Extensions/Date.swift
@@ -0,0 +1,8 @@
+//
+//  Date.swift
+//  SwiftfulCrypto
+//
+//  Created by jyotirmoy_halder on 31/8/25.
+//
+
+import Foundation
